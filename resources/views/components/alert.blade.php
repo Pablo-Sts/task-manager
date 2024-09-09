f65c5a4c10613a5fe974534429edd4ac003fe454
Pablo-Sts <pablo.santos0829@gmail.com>
2024-09-08 21:35:02 -0300

    Update alerts style

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,25 +1,33 @@
 @if (session()->has("success"))
-    <div>
-        {{ session("success") }}
+<div class="container-fluid p-3">
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-success text-dark fs-5 p-3">{{ session("success") }}</li>
+        </ul>
     </div>
 @endif
 
 @if (session()->has("message"))
-    <div>
-        {{ session("message") }}
+<div class="container-fluid p-3">
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-info text-dark fs-5 p-3">{{ session("message") }}</li>
+        </ul>
     </div>
 @endif
 
 @if (session()->has("errror"))
-    <div>
-        {{ session("error") }}
+<div class="container-fluid p-3">
+        <ul class="list-group">
+            <li class="list-group-item list-group-item-danger text-dark fs-5 p-3">{{ session("error") }}</li>
+        </ul>
     </div>
 @endif
 
 @if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
+    <div class="container-fluid p-3">
+        <ul class="list-group">
+            @foreach ($errors->all() as $error)
+                <li class="list-group-item list-group-item-danger text-danger fs-5 p-3">{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endif
